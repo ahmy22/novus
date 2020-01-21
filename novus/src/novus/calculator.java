fd290d386873def6a7528e39828bfb6ba1a57922
ahmy22 <60043210+ahmy22@users.noreply.github.com>
2020-01-21 10:52:13 +0000

    add calculator.java
    java class to try out junit testing

---
+++ novus/src/novus/calculator.java
@@ -0,0 +1,20 @@
+package novus;
+
+public class calculator {
+	
+	public int add (int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract (int a, int b) {
+		return a - b;
+	}
+	
+	public float division (int a, int b) {
+		return a/b;
+	}
+	
+	public int multiplication (int a, int b) {
+		return a*b;
+	}
+}
